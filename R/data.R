e73795ae5fe2257965a6f8549b50a305f4ff191b
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2019-04-24 14:39:02 +0200

    updated manual

--- R/data.R
+++ R/data.R
@@ -1,73 +1,35 @@
 #' SWAT model setups for \code{SWATplusR}
 #'
-#' \code{SWATdata} provides SWAT model setups for SWAT2012 and different SWAT+
-#' revisions for the gauge J of the Little River Experimental watershed (LREW,
-#' Bosch et al. 2007).
+#' \code{SWATdata} provides SWAT model setups for SWAT2012 and several SWAT+
+#' revisions for the gauge J of the Little River Experimental Watershed (LREW,
+#' \emph{Bosch et al.}, 2007).
 #'
-#' The following datasets are currently available from \code{SWATdata}:
-#'   \tabular{rccc}{
+#' To test the functionality of \code{SWATplusR}, \code{SWATdata} provides a
+#' variety of light weight, fast running SWAT model setups. To load the
+#' available demo data use the function \code{\link[SWATplusR]{load_demo}} from
+#' the \code{SWATplusR} package. The setup of all provided model setups was done
+#' for the same head watershed of the LREW, for all SWAT versions and revisions.
+#' Additionally, spatial data (subbasins, HRUs, river network) are available as
+#' shape files for all model setups. For some model revisions only Windows or
+#' Linux executables are available. Other revisions are available for both
+#' platforms. The following table gives an overview of the available datasets:
+#'
+#'   \tabular{lccc}{
 #'   \strong{Version} \tab \strong{Windows} \tab \strong{Linux} \tab \strong{Spatial} \cr
-#'    SWAT2012 Rev622 \tab X \tab   \tab X\cr
-#'    SWAT2012 Rev637 \tab   \tab X \tab X\cr
-#'    SWAT+    Rev 57 \tab X \tab   \tab X\cr
-#'    SWAT+    Rev 59 \tab X \tab X \tab X\cr
+#'    SWAT2012 Rev.622 \tab X \tab   \tab X\cr
+#'    SWAT2012 Rev.637 \tab   \tab X \tab X\cr
+#'    SWAT+    Rev.57 \tab X \tab   \tab X\cr
+#'    SWAT+    Rev.59 \tab X \tab X \tab X\cr
 #'      }
-#' @param a,b characters.
 #' @name SWATdata
 #' @references
 #' \cite{ Bosch, D. D., Sheridan, J. M., Lowrance, R. R., Hubbard, R. K.,
 #'        Strickland, T. C., Feyereisen, G. W. and Sullivan, D. G.: Little river
 #'        experimental watershed database, Water Resources Research, 43(9),
 #'        \url{https://doi.org/10.1029/2006wr005844}, 2007}
+#'
+#' @examples
+#' #
+#' library(SWATplusR)
+#'
 NULL
-
-#' Concatenate with an underscore
-#'
-#' \%_\% pastes two strings by "_".
-#' @rdname pasta
-#' @export
-"%_%" <- function(a, b) paste(a, b, sep = "_")
-
-#' Concatenate with a hyphen
-#'
-#' \%-\% pastes two strings by "-".
-#' @rdname pasta
-#' @export
-'%-%' <- function(a, b) paste(a, b, sep = "-")
-
-#' Concatenate with a dot
-#'
-#' \%.\% pastes two strings by ".".
-#' @rdname pasta
-#' @export
-'%.%' <- function(a, b) paste(a, b, sep = ".")
-
-#' Paste slash function
-#'
-#' \%//\% pastes two strings by "/".
-#' @rdname pasta
-#' @export
-'%//%' <- function(a, b) paste(a, b, sep = "/")
-
-#' Concatenate without separator
-#'
-#' \%&\% pastes two strings by "".
-#' @rdname pasta
-#' @export
-'%&%' <- function(a, b) paste0(a, b)
-
-#' Concatenate with space
-#'
-#' \%&&\% pastes two strings by " ".
-#' @rdname pasta
-#' @export
-'%&&%' <- function(a, b) paste(a, b, sep = " ")
-
-#' Concatenate with or
-#'
-#' \%|\% pastes two strings with a "|".
-#' @rdname pasta
-#' @export
-'%|%' <- function(a, b) paste(a, b, sep = "|")
-
-
